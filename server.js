1a5daa0083cba357fdb63cae34c60b23a8105ebb
Ian Lockhart <ianlockhart@Ians-MacBook-Pro.local>
2021-11-03 13:15:47 -0600

    db connected

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+require('dotenv').config()
 const MONGODB_URI = process.env.MONGODB_URI
 const db = mongoose.connection;
-require('dotenv').config()
+
 
 //___________________
 //Port
@@ -16,14 +17,14 @@ const recipesController = require('./controllers/recipes.js')
 app.use('/recipes', recipesController)
 
 
-
+console.log(MONGODB_URI);
 
 // Connect to Mongo
 mongoose.connect(MONGODB_URI  ,  { useNewUrlParser: true});
 
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 app.listen(3000, ()=>{
