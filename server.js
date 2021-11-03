97d092c38635ef87979d44dbbe4f6ed934afc5aa
Ian Lockhart <ianlockhart@Ians-MacBook-Pro.local>
2021-11-03 13:01:42 -0600

    working on db connection

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI
+const db = mongoose.connection;
+require('dotenv').config()
 
-const recipesController = require('.controllers/recipes.js')
+//___________________
+//Port
+//___________________
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3003;
+
+
+const recipesController = require('./controllers/recipes.js')
 app.use('/recipes', recipesController)
 
+
+
+
+// Connect to Mongo
+mongoose.connect(MONGODB_URI  ,  { useNewUrlParser: true});
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 app.listen(3000, ()=>{
     console.log('listening...');
