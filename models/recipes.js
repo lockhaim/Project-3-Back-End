67db7b1f18ec1be70c595077508ae09fe1afefac
Kevin Trang <ktrngcode@gmail.com>
2021-11-04 13:35:23 -0400

    updated .env file and server.js

--- models/recipes.js
+++ models/recipes.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const recipesSchema = new mongoose.Schema({
+    title: String,
     description: String,
+    ingredients: [String],
+    time: Number,
+    image: String,
     complete: Boolean
 });
 
